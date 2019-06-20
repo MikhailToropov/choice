31667481ff3bb25a994d364bdd5ce9d65320b620
MikhailToropov <42602387+MikhailToropov@users.noreply.github.com>
2019-06-20 16:36:49 +0200

    add answer yes

--- choice.py
+++ choice.py
@@ -2,7 +2,20 @@
 from random import randint
 from time import sleep
 def main():
-	print("Program")
+	answer_yes = """
+    YY    YY   EEEEEE    SSSS
+    YY    YY   EEEEEE   SSSSSS
+     YY  YY    EE       SS
+     YY  YY    EE       SS
+       YY      EEEEE    SSSSS
+       YY      EEEEE     SSSSS
+       YY      EE           SS 
+       YY      EE           SS
+       YY      EEEEEE   SSSSSS
+       YY      EEEEEE    SSSS 
+    """
+	print(answer_yes)
 	return 0
 if __name__ == "__main__":
 	print("Start")
+	main()
